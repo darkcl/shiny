6d356bc36748b6fc25789536e845968108640c47
Yeung Yiu Hung <hkclex@gmail.com>
2017-12-27 01:35:16 +0800

    Complete basic function

--- shiny.py
+++ shiny.py
@@ -4,6 +4,61 @@ import sqlite3
 
 # Database related
 
+def mark_done(name, path):
+    """Mark Done in progress database
+
+    Args:
+        name (str): Name of the pokemon
+        path (str): Path of progress database
+    """
+    conn = sqlite3.connect(path)
+    cur = conn.cursor()
+    result = cur.execute("""
+        SELECT EXISTS (SELECT 1 FROM counter WHERE PKM_NAME=? LIMIT 1);
+    """, (name, ))
+
+    for row in result:
+        if row[0] == 0:
+            click.echo("%s not exist" % name)
+        else:
+            cur.execute("""
+                UPDATE `counter` SET `DONE`= 1  WHERE `PKM_NAME`= ?;
+            """, (name, ))
+            click.echo("Complete hunt for %s" % name)
+        break
+    conn.commit()
+    conn.close()
+
+def add_counter(name, increase_by, path):
+    """Add counter in progress database
+
+    Args:
+        name (str): Name of the pokemon
+        increase_by (int): Adding count
+        path (str): Path of progress database
+    """
+    conn = sqlite3.connect(path)
+    cur = conn.cursor()
+    result = cur.execute("""
+        SELECT EXISTS (SELECT 1 FROM counter WHERE PKM_NAME=? LIMIT 1);
+    """, (name, ))
+
+    for row in result:
+        if row[0] == 0:
+            cur.execute("INSERT INTO `counter`(`ID`,`PKM_NAME`) VALUES (NULL, ?);", (name, ))
+            cur.execute("""
+                UPDATE `counter` SET `ENCOUNTER`= `ENCOUNTER` + %i  WHERE `PKM_NAME`= ?;
+            """ % increase_by, (name, ))
+            click.echo("New encounter for %s with %i time" % (name, increase_by))
+        else:
+            cur.execute("""
+                UPDATE `counter` SET `ENCOUNTER`= `ENCOUNTER` + %i  WHERE `PKM_NAME`= ?;
+            """ % increase_by, (name, ))
+            click.echo("Encounter %s for %i time" % (name, increase_by))
+        break
+    conn.commit()
+    conn.close()
+
 def create_hunt(name, path):
     """Create Hunt Record for progress database
 
@@ -68,24 +123,26 @@ def hunt(names):
     """
     pkm_name = names.split()
     path = os.path.join(os.getcwd(), 'hunt.sqlite')
-
-    click.echo('Start Hunting %s' % ", ".join(pkm_name))
     initalize_database(path)
-
     for pkm in pkm_name:
         create_hunt(pkm, path)
 
 
 @click.command()
+@click.option('--add', default=1, help='Number of encounters')
 @click.argument('names')
-def count(names):
+def count(add, names):
     """Add encounter to hunting target.
 
     Args:
         names (str): Name(s) of the pokemon. Space as separtor.
 
     """
-    click.echo('Add encounter to counter')
+    pkm_name = names.split()
+    path = os.path.join(os.getcwd(), 'hunt.sqlite')
+    initalize_database(path)
+    for pkm in pkm_name:
+        add_counter(pkm, add, path)
 
 @click.command()
 @click.argument('names')
@@ -96,7 +153,11 @@ def get(names):
         names (str): Name(s) of the pokemon. Space as separtor.
 
     """
-    click.echo('Complete hunt for ')
+    pkm_name = names.split()
+    path = os.path.join(os.getcwd(), 'hunt.sqlite')
+    initalize_database(path)
+    for pkm in pkm_name:
+        mark_done(pkm, path)
 
 cli.add_command(hunt)
 cli.add_command(count)
