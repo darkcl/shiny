dfe17e86cc561ddea1959030299b2795e051b856
Yeung Yiu Hung <hkclex@gmail.com>
2017-12-26 23:40:54 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="shiny",
+    version='0.1',
+    py_modules=['shiny'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        shiny=shiny:cli
+    ''',
+)
