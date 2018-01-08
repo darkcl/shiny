f2feaecbb06b15517a69b939bad96595736683c7
Yeung Yiu Hung <hkclex@gmail.com>
2018-01-09 02:00:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="shiny",
     version='0.2',
     py_modules=['cli', 'core', 'web'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click',
         'terminaltables',
